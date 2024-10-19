5b202a600d329d47ee9c65f971cf2e4bc7f0c413
Seyidmammadli_i <Seyidmammadli_i@STHQ0121-01>
2024-10-19 11:12:12 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+var listener = new Socket(AddressFamily.InterNetwork,
+                          SocketType.Dgram,
+                          ProtocolType.Udp);
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 45678;
+var listenerEP = new IPEndPoint(ip, port);
+
+listener.Bind(listenerEP);
+
+var buffer = new byte[ushort.MaxValue - 28];
+EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+
+var count = 0;
+var msg = string.Empty;
+
+
+
+//while (true)
+//{
+//    count = listener.ReceiveFrom(buffer, ref remoteEP);
+//    msg = Encoding.Default.GetString(buffer, 0, count);
+//    Console.WriteLine($"{remoteEP} : {msg}");
+//}
+
+
+
+
+
+while (true)
+{
+    var result = await listener.ReceiveFromAsync(new ArraySegment<byte>(buffer),
+                                                 SocketFlags.None,
+                                                 remoteEP);
+
+    count = result.ReceivedBytes;
+    msg = Encoding.Default.GetString(buffer, 0, count);
+    Console.WriteLine($"{result.RemoteEndPoint} : {msg}");
+}
